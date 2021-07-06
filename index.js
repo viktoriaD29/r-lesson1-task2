a6ddfa0438ce53f7b60d3c75d8d7f9cd8c19bd54
Viktoria <doduchenko29@gmail.com>
2021-07-06 08:59:51 +0300

    add

---
+++ index.js
@@ -0,0 +1,14 @@
+const rootElement = document.querySelector('#root');
+
+const element = React.createElement(
+  'div',
+  { className: 'greeting' },
+  React.createElement('h1', { className: 'greeting__title' }, 'Hello, world!'),
+  React.createElement(
+    'p',
+    { className: 'greeting__text' },
+    "I'm learning React"
+  )
+);
+
+ReactDOM.render(element, rootElement)
